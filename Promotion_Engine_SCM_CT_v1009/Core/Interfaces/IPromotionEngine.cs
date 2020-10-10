f9b341f2e6143c2a40d4d8378771b37f0f1f19bd
Alexander Falk <alexfalk7@gmail.com>
2020-10-10 14:30:39 +0200

    Started writing the interface containing functionality to be used by the Promotion Engine

---
+++ Promotion_Engine_SCM_CT_v1009/Core/Interfaces/IPromotionEngine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Promotion_Engine_SCM_CT_v1009.Models;
+using Promotion_Engine_SCM_CT_v1009.Models.DTO;
+
+namespace Promotion_Engine_SCM_CT_v1009.Core.Interfaces
+{
+    public interface IPromotionEngine
+    {
+        /// <summary>
+        /// Using this method, you are able to add one or more of a SKU to a cart
+        /// </summary>
+        /// <param name="sku">The SKU to be added</param>
+        /// <param name="count">The number of SKUs needed. Default is 1</param>
+        void AddSkuToCart(SKUEnum sku, int count = 1);
+
+        /// <summary>
+        /// Deletes one SKU from the cart
+        /// </summary>
+        /// <param name="sku">The SKU to be deleted</param>
+        void DeleteSkuFromCart(SKUEnum sku);
+
+        /// <summary>
+        /// Gets the price of a SKU
+        /// </summary>
+        /// <param name="sku">The SKU to retrieve the price for</param>
+        /// <returns>The price of the specified SKU</returns>
+        double GetSkuPrice(SKUEnum sku);
+
+        /// <summary>
+        /// Gets the price of a SKU
+        /// </summary>
+        /// <param name="sku">The SKU to retrieve the price for</param>
+        /// <returns>The price of the specified SKU</returns>
+        List<PromotionDTO> GetPromotions();
+    }
+}
---
+++ Promotion_Engine_SCM_CT_v1009/Models/DTO/PromotionDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Promotion_Engine_SCM_CT_v1009.Models.DTO
+{
+    /// <summary>
+    /// This class is a Data Transfer Object used to pass around promotion data
+    /// </summary>
+    public class PromotionDTO
+    {
+        public PromotionType PromotionType { get; set; }
+        public Promotion Promotion { get; set; }
+        public PromotionDiscount PromotionDiscount { get; set; }
+    }
+}
